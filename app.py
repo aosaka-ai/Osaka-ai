78a6564c0be0c2ab42109e9ab171f8e6e16a0a30
aosaka-ai <a.osaka@gmail.com>
2026-04-08 13:59:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ if "client" not in st.session_state:
 )
 def safe_generate_content(user_input):
     return st.session_state.client.models.generate_content(
-        # model="gemini-2.0-flash", 
-        model="gemini-1.5-flash-8b",
+        model="gemini-2.0-flash", 
+        #model="gemini-1.5-flash-8b",
         contents=user_input,
         config=types.GenerateContentConfig(
             system_instruction="أنت Osaka AI، مساعد تقني خبير لبنك NBE. رد بلهجة مصرية تقنية."
